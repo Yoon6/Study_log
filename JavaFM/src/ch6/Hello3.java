c73ca5ef108774e24b6675fd728c1761ae73df3e
YoonKraken <yoonda5898@gmail.com>
2020-09-15 19:28:35 +0900

    AndroidDoit
    둘째 마당-01

---
+++ JavaFM/src/ch6/Hello3.java
@@ -0,0 +1,13 @@
+package ch6;
+// 한 소스파일에 여러개의 클래스 작성 가능
+// 한 소스파일에 하나의 클래스만 작성하는게 바람직함
+
+class Hello2 { // public 클래스 이름과 소스파일 이름과 일치해야함
+}
+//public class Hello3{} // 하나의 소스파일에는 하나의 public class만 허용
+class hello3{
+    public static void main(String[] args) {
+        System.out.println("22");
+    }
+}
+class Hello4{}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Study_log
-programming study
+--- 
+### JavaFM
+
+학습(참고) 도서 : 자바의 정석 3판
+[정리 내용](https://www.notion.so/yoon6/Study_Java-b31feaf8f36144e584d80d5b65713de0)
+
+### AndroidDoit
+
+학습(참고) 도서 : Doit! 안드로이드 앱 프로그래밍 개정 6판
+[정리 내용](https://www.notion.so/yoon6/Study_Android-956b9a1349b944d6bf91c66e12f38b47)
